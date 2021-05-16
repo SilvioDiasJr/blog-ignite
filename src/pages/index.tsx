c99e4d883144c1df95a2c5ea7779123196d60ac0
Silvio <silvio.dias@etec.sp.gov.br>
2021-05-16 16:11:57 -0300

    adicionando o componente header e a biblioteca dns

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import { GetStaticProps } from 'next';
 import Prismic from '@prismicio/client'
@@ -8,7 +9,8 @@ import ptBR from 'date-fns/locale/pt-BR'
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
-import { useState } from 'react';
+import Link from 'next/link';
+import Header from '../components/Header';
 
 interface Post {
   uid?: string;
@@ -62,30 +64,39 @@ export default function Home({ postsPagination }: HomeProps) {
         <title>Posts | spacetraveling.</title>
       </Head>
 
+      <Header />
       <main className={commonStyles.containerContent}>
         {posts.map(post => (
-          <div className={styles.postCard}>
-            <a href=''>
-              <h1>{post.data.title}</h1>
-              <p>{post.data.subtitle}</p>
+          <div
+            className={styles.postCard}
+            key={post.uid}
+          >
+            <Link href={`/post/${post.uid}`}>
+              <a >
+                <h1>{post.data.title}</h1>
+                <p>{post.data.subtitle}</p>
 
-              <div className={styles.postCardFooter}>
-                <span>
-                  <FiCalendar />
-                  {post.first_publication_date}
-                </span>
-                <span>
-                  <FiUser />
-                  {post.data.author}
-                </span>
-              </div>
-            </a>
+                <div className={styles.postCardFooter}>
+                  <span>
+                    <FiCalendar />
+                    {format(new Date(post.first_publication_date),
+                      'dd MMM yyyy',
+                      { locale: ptBR }
+                    )}
+                  </span>
+                  <span>
+                    <FiUser />
+                    {post.data.author}
+                  </span>
+                </div>
+              </a>
+            </Link>
           </div>
         ))}
 
         {pagination && (
           <a
-          className={styles.buttonMorePosts}
+            className={styles.buttonMorePosts}
             onClick={() => handleMorePost()}
           >
             Carregar mais posts
@@ -109,10 +120,7 @@ export const getStaticProps: GetStaticProps = async () => {
   const posts = postsResponse.results.map(post => {
     return {
       uid: post.uid,
-      first_publication_date: format(new Date(post.first_publication_date),
-        'dd MMM yyyy',
-        { locale: ptBR }
-      ),
+      first_publication_date: post.first_publication_date,
       data: {
         title: post.data.title,
         subtitle: post.data.subtitle,
