fa8f06b42056092d21c209f889f9fb4453b83deb
Silvio <silvio.dias@etec.sp.gov.br>
2021-05-16 16:08:47 -0300

    implementando o link da home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,14 @@
+import Link from 'next/link'
 import styles from './header.module.scss'
 
 export default function Header() {
   return (
     <header className={styles.container}>
-      <img src="/images/logo.png" alt="logo" />
+      <Link href='/'>
+        <a>
+          <img src="/images/logo.png" alt="logo" />
+        </a>
+      </Link>
     </header>
   )
 }
