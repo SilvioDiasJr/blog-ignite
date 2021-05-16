5463043dee9962e338050584c4b6d14569eeb9f0
Silvio <silvio.dias@etec.sp.gov.br>
2021-05-16 16:48:05 -0300

    alterando a funcao de formatacao dns

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,11 +42,7 @@ export default function Home({ postsPagination }: HomeProps) {
         const newPost = data.results.map((post: Post) => {
           return {
             uid: post.uid,
-            first_publication_date:
-              format(new Date(post.first_publication_date),
-                'dd MMM yyyy',
-                { locale: ptBR }
-              ),
+            first_publication_date: post.first_publication_date,
             data: {
               title: post.data.title,
               subtitle: post.data.subtitle,
